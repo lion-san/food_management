cfbd3cbdc85667a118eb38c5a2652d999f2873ab
yokoi.shinya <shinya.yokoi@gmail.com>
2015-06-17 10:50:14 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nofoodloss::Application.config.secret_key_base = 'c4ee69b30b83ed38766960424119ed6b30c51cf1233c475d46b5026afa73da8ef6e5dfadc072424ebf5301a94f38737bd4a26cef1781b772717eddfb7491c729'
