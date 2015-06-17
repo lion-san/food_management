5fc9ddd119ce08bb3dcd702303c2f4ef88b26931
yokoi.shinya <shinya.yokoi@gmail.com>
2015-06-17 11:15:24 +0900

    Initialize

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 Nofoodloss::Application.config.secret_key_base = 'c4ee69b30b83ed38766960424119ed6b30c51cf1233c475d46b5026afa73da8ef6e5dfadc072424ebf5301a94f38737bd4a26cef1781b772717eddfb7491c729'
