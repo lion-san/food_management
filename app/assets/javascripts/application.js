a1f33d02bbdb947c7cd0ba53632d7e45bda075b5
yokoi.shinya <shinya.yokoi@gmail.com>
2015-06-18 10:22:55 +0900

    navigation, item#index create

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//
+//= require jquery-2.1.0.min
 //= require turbolinks
 //= require_tree .
