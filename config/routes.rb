8e19a7d236b720684a5bd6e47cf9fdd84882b49a
m.yokota <yokota.mmy.1013@gmail.com>
2015-06-22 16:21:50 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Nofoodloss::Application.routes.draw do
   resources :items
   get "static_pages/home"
   get "static_pages/help"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
