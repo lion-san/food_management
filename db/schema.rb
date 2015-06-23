a699659efdeed2361b0785fe6291aefedd6549af
yokoi.shinya <shinya.yokoi@gmail.com>
2015-06-23 17:15:56 +0900

    user.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622071605) do
+ActiveRecord::Schema.define(version: 20150623064455) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -112,6 +112,9 @@ ActiveRecord::Schema.define(version: 20150622071605) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
